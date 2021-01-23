294cd0716c26c668abc7e9bd6bf30fbcd930a34a
dungbk <conandung9999@gmail.com>
2021-01-23 21:58:40 +0700

    create a class [94]

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -29,17 +30,25 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = ['You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
+  // List<String> questions = ['You can lead a cow down stairs but not up stairs.',
+  //   'Approximately one quarter of human bones are in the feet.',
+  //   'A slug\'s blood is green.',
+  // ];
+  // List<bool> answers = [false, true, true];
+
+  // Question q1 = Question(q: 'You can lead a cow down stairs but not up stairs.', a: false);
+
+  List<Question> questions = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
   ];
-  List<bool> answers = [false, true, true];
 
   int questionNum = 0;
 
   void checkAnswer(bool ans) {
     setState(() {
-      if(answers[questionNum] == ans) {
+      if(questions[questionNum].answer == ans) {
         scoreKeeper.add(Icon(Icons.check, color: Colors.green,));
       } else {
         scoreKeeper.add(Icon(Icons.close, color: Colors.red,));
@@ -69,7 +78,7 @@ class _QuizPageState extends State<QuizPage> {
             child: Center(
               child: Text(
                 // 'This is where the question text will go.',
-                questions[questionNum],
+                questions[questionNum].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
